8476fbb7e17e01995d53b40e998fa7c52c8ec843
Joanna Wolska <joanna.maria.wolska@gmail.com>
2022-12-15 19:21:46 +0100

    added WeekDays class and unit tests

---
+++ homeworkJAVA/src/main/java/lesson2task6/WeekDays.java
@@ -0,0 +1,23 @@
+package lesson2task6;
+
+public class WeekDays {
+    public static String getDayOfWeek(int dayOfWeek) {
+        switch (dayOfWeek) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+            case 7:
+                return "Weekend";
+            default:
+                return "There is no such a day";
+        }
+    }
+}
---
+++ homeworkJAVA/src/test/java/lesson2task6/WeekDaysTest.java
@@ -0,0 +1,24 @@
+package lesson2task6;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class WeekDaysTest {
+
+    @Test
+    void shouldBeOneOfWorkingDay() {
+        String getDayOfWeek = WeekDays.getDayOfWeek(1);
+        Assertions.assertEquals("Monday", getDayOfWeek);
+    }
+    @Test
+    void shouldBeWeekendDay() {
+        String getDayOfWeek = WeekDays.getDayOfWeek(6);
+        Assertions.assertEquals("Weekend", getDayOfWeek);
+    }
+    @Test
+    void shouldBeNoSuchDay() {
+        String getDayOfWeek = WeekDays.getDayOfWeek(10);
+        Assertions.assertEquals("There is no such a day", getDayOfWeek);
+    }
+}
