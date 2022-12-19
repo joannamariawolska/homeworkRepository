e1b4a951ad7d415acb8ca79d64684d04440b29ab
Joanna Wolska <joanna.maria.wolska@gmail.com>
2022-12-19 20:17:51 +0100

    corrected

--- homeworkJAVAlesson3/src/main/java/lesson3task1/Main.java
+++ homeworkJAVAlesson3/src/main/java/lesson3task1/Main.java
@@ -2,7 +2,7 @@ package lesson3task1;
 
 public class Main {
     public static void main(String[] args){
-        SeatAtCinema seatAtCinema = new SeatAtCinema(5, 1, true);
+        SeatAtCinema seatAtCinema = new SeatAtCinema(5, 1);
 
         System.out.println(seatAtCinema.getMeMySeat());
     }
