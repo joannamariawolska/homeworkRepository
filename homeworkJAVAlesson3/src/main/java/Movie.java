1d579176212137aba6eba9442b54dc7044b9d5ce
Joanna Wolska <joanna.maria.wolska@gmail.com>
2022-12-20 16:51:05 +0100

    corrected

--- homeworkJAVAlesson3/src/main/java/Movie.java
+++ homeworkJAVAlesson3/src/main/java/Movie.java
@@ -5,11 +5,11 @@ public class Movie {
     private final int lengthInMinutes;
     private final int yearOfProduction;
 
-    protected String getDescriptionOfMovie(){
+    public String getDescriptionOfMovie(){
         return "Twój film to " + title + " z " + yearOfProduction + ". " + "Wyreżyserował " + director  + ". " + "Długość filmu to " + lengthInMinutes + " minuty.";
     }
 
-    protected Movie(String title, String director, int lengthInMinutes, int yearOfProduction) {
+     public Movie(String title, String director, int lengthInMinutes, int yearOfProduction) {
         this.title = title;
         this.director = director;
         this.lengthInMinutes = lengthInMinutes;
